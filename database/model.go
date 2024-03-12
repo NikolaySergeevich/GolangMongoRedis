f8d449b64b8312c78a03823f1df32b69427c6b06
Nikita <robotomize@gmail.com>
2024-03-12 19:06:55 +0000

    Update model.go

--- database/model.go
+++ database/model.go
@@ -9,6 +9,5 @@ import (
 type Command struct {
 	ID        uuid.UUID `json:"id" bson:"id"`
 	Command   string    `json:"command" bson:"command"`
-	Args      []string  `json:"args" bson:"args"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
